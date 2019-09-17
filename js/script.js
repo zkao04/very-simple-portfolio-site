4f48f5cdd55d817e1bfb04a38a513e4280fbb537
Hines Kao <hineskao@gmail.com>
2019-09-17 13:05:07 -0700

    Initial Setup Completed

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+// Usage is copied and paste from Superslides Github Page
+  $('#slides').superslides({
+    animation:"fade",
+    play:5000,
+    pagination:false
+  });
+
+});
