bb18afdeb35d1c8d8a1c77d29086a3ef74832e69
Hines Kao <hineskao@gmail.com>
2019-09-18 08:33:20 -0700

    Completed about section, starting on skills

--- js/script.js
+++ js/script.js
@@ -6,4 +6,36 @@ $(document).ready(function() {
     pagination:false
   });
 
+  var typed = new Typed(".typed", {
+    strings: ["Full Stack.", "Web Developer.", "Wannabe Writer."],
+    typeSpeed: 70,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false
+
+  });
+
+  $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:10,
+      nav:true,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:3
+          },
+          1000:{
+              items:5
+          }
+      }
+  });
+
+
+  $('.chart').easyPieChart({
+      //your options goes here
+  });
+
+
 });
